879fbdc8598de291f9daef14676e0b2c49f224f8
Mark Johnson <Mark.Johnson.62899@gmail.com>
2017-11-04 17:27:54 -0500

    Perl rewrite

---
+++ mark-installs.pl
@@ -0,0 +1,23 @@
+#!/usr/bin/perl
+use strict;
+die "Must be run as root\n" if `id -u`!=0;
+`removepkg $ARGV[1]; rm /home/PACKAGES/$ARGV[1]*` and exit if $ARGV[0] eq '-u';
+my ($auto,$clean,$pkg,$input,@srcs)=($ARGV[0] eq '-a',1,"","",());
+$pkg=$ARGV[$auto];
+die if system("tar -xf $pkg.tar.gz")!=0;
+`cp $pkg*.tar.gz $pkg/`;
+if (not $auto) {
+	do {
+		print `ls -l`,"More source files? (Blank line if none)\n";
+		chop($input=<STDIN>);
+		`cp $input $pkg/` and push @srcs,$input unless $input eq '';
+	} while (not $input eq '');
+}
+die if system("cd $pkg && ./$pkg.SlackBuild")!=0;
+`installpkg /tmp/$pkg*.t?z && mv /tmp/$pkg*.t?z /home/PACKAGES/`;
+print "Clean up? (Y/n): " and chop($clean=<STDIN>) unless $auto;
+if (not $clean||$clean=~/[Yy]/) {
+	`rm -rf $pkg*`;
+	`rm $_` for (@srcs);
+}
+print "Installation complete\n";
