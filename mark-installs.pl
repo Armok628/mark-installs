60301d2a0e5488e18d415e16bd2fcca98bb7f231
Mark Johnson <Mark.Johnson.62899@gmail.com>
2017-11-04 20:23:57 -0500

    Better style

--- mark-installs.pl
+++ mark-installs.pl
@@ -1,23 +1,26 @@
 #!/usr/bin/perl
 use strict;
-die "Must be run as root\n" if `id -u`!=0;
-`removepkg $ARGV[1]; rm /home/PACKAGES/$ARGV[1]*`, exit if $ARGV[0] eq '-u';
+use warnings;
+`id -u`==0 or die "Must be run as root\n";
+@ARGV or die "Must specify package\n";
+system("removepkg $ARGV[1]; rm /home/PACKAGES/$ARGV[1]*"), exit if $ARGV[0] eq '-u';
 my ($auto,$clean,$pkg,$input,@srcs)=($ARGV[0] eq '-a',1,"","",());
 $pkg=$ARGV[$auto];
-die if system("tar -xf $pkg.*")!=0;
-`cp $pkg*.* $pkg/`;
+die if system("tar -xf $pkg.*");
+system("cp $pkg*.* $pkg/");
 if (not $auto) {
 	do {
-		print `ls -l`,"More source files? (Blank line if none)\n";
+		system("ls -l");
+		print "More sources? (Blank line if none)\n";
 		chop($input=<STDIN>);
-		`cp $input $pkg/` and push @srcs,$input unless $input eq '';
-	} while (not $input eq '');
+		push @srcs,$input if ($input && !system("cp $input $pkg/"));
+	} while $input;
 }
-die if system("cd $pkg && ./$pkg.SlackBuild")!=0;
-`installpkg /tmp/$pkg*.t?z && mv /tmp/$pkg*.t?z /home/PACKAGES/`;
+die if system("cd $pkg && ./$pkg.SlackBuild");
+system("installpkg /tmp/$pkg*.t?z && mv /tmp/$pkg*.t?z /home/PACKAGES/");
 print "Clean up? (Y/n): " and chop($clean=<STDIN>) unless $auto;
 if ($auto||$clean=~/[Yy]/||not $clean) {
-	`rm -rf $pkg*`;
-	`rm $_` for (@srcs);
+	system("rm -rf $pkg*");
+	system("rm $_") for (@srcs);
 }
 print "Installation complete\n";
